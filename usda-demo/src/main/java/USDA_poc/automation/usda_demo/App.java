9894c24a84a8c65db4bba471fa6cc1dd33ae2f34
Tuniki, Arun (CTR) - RD, Washington, DC <Arun.Tuniki@usda.gov>
2024-07-24 22:32:26 -0400

    USDA demo commit

---
+++ usda-demo/src/main/java/USDA_poc/automation/usda_demo/App.java
@@ -0,0 +1,13 @@
+package USDA_poc.automation.usda_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
